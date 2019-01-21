e0b97c30741ebddaef5c835cf37a6df76c813a16
Alex Dietsche <dietscha@ethz.ch>
2019-01-21 22:20:49 +0100

    add visualization with slider WIP

---
+++ leg_analysis/colissions/leg_slider.m
@@ -0,0 +1,99 @@
+function varargout = leg_slider(varargin)
+% LEG_SLIDER MATLAB code for leg_slider.fig
+%      LEG_SLIDER, by itself, creates a new LEG_SLIDER or raises the existing
+%      singleton*.
+%
+%      H = LEG_SLIDER returns the handle to a new LEG_SLIDER or the handle to
+%      the existing singleton*.
+%
+%      LEG_SLIDER('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in LEG_SLIDER.M with the given input arguments.
+%
+%      LEG_SLIDER('Property','Value',...) creates a new LEG_SLIDER or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before leg_slider_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to leg_slider_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help leg_slider
+
+% Last Modified by GUIDE v2.5 21-Jan-2019 22:02:32
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @leg_slider_OpeningFcn, ...
+                   'gui_OutputFcn',  @leg_slider_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before leg_slider is made visible.
+function leg_slider_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to leg_slider (see VARARGIN)
+
+% Choose default command line output for leg_slider
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes leg_slider wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = leg_slider_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+k = get(handles.slider1, 'Value');
+theta = 
+update_viualization(theta);
+
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
